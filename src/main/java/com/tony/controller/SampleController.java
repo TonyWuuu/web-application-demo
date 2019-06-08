75824b40dc8f401eda733877ccc86792a56a1ee0
tony.wu <tonywuuu@hotmail.com>
2019-06-08 21:34:54 +0800

    sample controller

--- src/main/java/com/tony/controller/SampleController.java
+++ src/main/java/com/tony/controller/SampleController.java
@@ -1,6 +1,5 @@
-package controller;
+package com.tony.controller;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,4 @@ public class SampleController {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
-    }
 }
