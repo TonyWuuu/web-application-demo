c3de09cbd61ae797ff769806fd36a13c40197bef
tony.wu <tonywuuu@hotmail.com>
2019-06-10 21:28:50 +0800

    h2 + greeting page

--- src/main/java/com/tony/controller/SampleController.java
+++ src/main/java/com/tony/controller/SampleController.java
@@ -2,16 +2,15 @@ package com.tony.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    @GetMapping("/greeting")
+    public String index() {
+        return "greeting.html";
     }
 
 }
