36b67e8214ab222b401cbff857dc0f2251090c7b
tony.wu <tonywuuu@hotmail.com>
2019-06-11 21:01:56 +0800

    add jsp support

--- src/main/java/com/tony/controller/SampleController.java
+++ src/main/java/com/tony/controller/SampleController.java
@@ -2,15 +2,24 @@ package com.tony.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-    @GetMapping("/greeting")
+    @RequestMapping("/greeting")
     public String greeting() {
         return "greeting.html";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
     }
 
 }
